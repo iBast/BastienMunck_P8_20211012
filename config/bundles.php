046591208740223eef9af7fbb1bfffcfc2ca1faf
Bastien <bastien.munck@me.com>
2021-10-19 15:16:41 +0200

    install flex

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+];
