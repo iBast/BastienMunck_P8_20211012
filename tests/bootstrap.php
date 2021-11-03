b127687d2e24cbb4f978f7f07afeb652ed96662b
Bastien <bastien.munck@me.com>
2021-11-03 15:18:51 +0100

    bootstrap configuration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,26 @@
 <?php
 
-use Symfony\Component\Dotenv\Dotenv;
-
-require dirname(__DIR__).'/vendor/autoload.php';
-
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+// clearing the cach
+if (isset($_ENV['BOOTSTRAP_CLEAR_CACHE_ENV'])) {
+    // executes the "php bin/console cache:clear" command
+    passthru(sprintf(
+        'APP_ENV=%s php "%s/../bin/console" cache:clear --no-warmup',
+        $_ENV['BOOTSTRAP_CLEAR_CACHE_ENV'],
+        __DIR__
+    ));
 }
+
+// refresh the DB
+if (isset($_ENV['BOOTSTRAP_FIXTURES_LOAD'])) {
+    $command = 'php ' . dirname(__DIR__, 1) . '/bin/console doctrine:database:drop --force --env=test';
+    passthru($command);
+    $command = 'php ' . dirname(__DIR__, 1) . '/bin/console doctrine:database:create --env=test';
+    passthru($command);
+    $command = 'php ' . dirname(__DIR__, 1) . '/bin/console doctrine:schema:create --env=test';
+    passthru($command);
+    // executes the "php bin/console cache:clear" command
+    $command = 'php ' . dirname(__DIR__, 1) . '/bin/console doctrine:fixtures:load -n --env=test';
+    passthru($command);
+}
+
+require __DIR__ . '/../config/bootstrap.php';
