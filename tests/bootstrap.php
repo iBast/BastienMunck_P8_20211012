55d11e568cb8dc235cf99197185f9c3446f49683
Bastien <bastien.munck@me.com>
2021-11-04 09:47:10 +0100

    security controller tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,7 +18,6 @@ if (isset($_ENV['BOOTSTRAP_FIXTURES_LOAD'])) {
     passthru($command);
     $command = 'php ' . dirname(__DIR__, 1) . '/bin/console doctrine:schema:create --env=test';
     passthru($command);
-    // executes the "php bin/console cache:clear" command
     $command = 'php ' . dirname(__DIR__, 1) . '/bin/console doctrine:fixtures:load -n --env=test';
     passthru($command);
 }
